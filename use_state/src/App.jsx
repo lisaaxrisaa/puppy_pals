5d7459e20e8b45c72e43091b0ed992a4525661d5
Lisa Fujita <lisafujita90@gmail.com>
2024-10-22 15:46:49 -0400

    puppy data with usestate

--- use_state/src/App.jsx
+++ use_state/src/App.jsx
@@ -1,14 +1,20 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 // import reactLogo from './assets/react.svg';
 // import viteLogo from '/vite.svg';
 // import './App.css';
+import { puppyList } from './data.js';
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log('puppyList: ', puppyList);
 
   return (
     <>
-      <div></div>
+      <div className="App">
+        {puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })}
+      </div>
     </>
   );
 }
